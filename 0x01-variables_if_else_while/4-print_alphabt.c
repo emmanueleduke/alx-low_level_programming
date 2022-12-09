194ee1ac590e9188837f261e6b7577c6afa60e9c
emmanueleduke <eduke.emmanuel@gmail.com>
2022-12-09 12:21:44 -0800

    Printing with lowercase with new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	char c;
-	char d;
 
 	c = 'a';
-	d = 'A';
+
 	while (c <= 'z')
 
 	{
-		putchar(c);
-		
-	}
-	while (d <= 'z')
+	if ((c != 'q' && c != 'e') && c <= 'z')
 
 	{
-		putchar(d);
-		d++;
+		putchar(c);
+		c++;
 	}
+	}
+	putchar('\n');
 	return (0);
 }
