34d98d00d9d0062a1912120d1a65b338b794a509
emmanueleduke <eduke.emmanuel@gmail.com>
2022-12-09 08:24:36 -0800

    Printing alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,12 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while 
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	putchar('\n');
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
 	return (0);
 }
