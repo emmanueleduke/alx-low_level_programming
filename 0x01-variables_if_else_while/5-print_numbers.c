d87cba935b78bdb377531a5e1f59b6b8c74bdec9
emmanueleduke <eduke.emmanuel@gmail.com>
2022-12-09 12:34:07 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+
+	{
+	printf("%d", a);
+	putchar(a);
+	}
+	return (0);
+}
