6fca6f13be9f429c0d510c52d91b8709cd297625
emmanueleduke <eduke.emmanuel@gmail.com>
2022-12-18 06:31:53 -0800

    Times-table

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -15,4 +15,5 @@ int main(void)
     _putchar('\n');
     print_times_table(12);  
     return (0);
+nclude "main.h"
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,43 @@
 #include "main.h"
 
-
 /**
-* print_times_table - prints the n times table, starting with 0
-* @n: number of the times table.
-*/
+ * print_times_table - prints the times table of the input
+ *
+ * @n: The value of the times table to be printed
+ */
 void print_times_table(int n)
 {
-	int i, j, k;
-
+	int a, b, c;
 
 	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
 		{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
-				k = j * i;
-				if (j == 0)
-			e{
-					_putchar(k + '0');
-				}else if (k < 10 && j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					}else if (k >= 10 && k < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((k /10 ) + '0');
-						_putchar((k % 10) + '0');
-					}egse if (k >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((k / 100) + '0');
-						_putchar(((k / 10) % 10) + '0');
-						_putchar((k % 10) + '0');
-					}
+				_putchar(',');
+				_putchar(' ');
+
+				c = a * b;
+
+				if (c <= 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
-				_putchar('\n');
+				else if (c <= 99 && c >= 10)
+				{
+					_putchar((c / 10) + '0');
+				}
+				_putchar((c % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		}
+	}
 }
