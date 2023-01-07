0a25cb47083d858c21733596c7cda2241abfd1ea
emmanueleduke <eduke.emmanuel@gmail.com>
2023-01-07 12:44:02 -0800

    Multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,12 +3,12 @@
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
+*
 * Return: the int converted from the string
 */
-
 int _atoi(char *s)
-{
 
+{
 	int i, d, n, len, f, digit;
 
 	i = 0;
@@ -17,9 +17,8 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-
 	while (s[len] != '\0')
-		len++;
+	len++;
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -28,58 +27,43 @@ int _atoi(char *s)
 		{
 			digit = s[i] - '0';
 		if (d % 2)
-		{
 			digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-		}
 		if (s[i + 1] < '0' || s[i + 1] > '9')
-		{
+
 			break;
 			f = 0;
-		}
 		}
 		i++;
 
 	}
-	if (f == 0)
-	{
-		return (0);
-	}
+		if (f == 0)
+
+			return (0);
 	return (n);
+
 }
-
-
-
 /**
 * main - multiplies two numbers
 * @argc: number of arguments
 * @argv: array of arguments
+*
 * Return: 0 (Success), 1 (Error)
 */
 
 int main(int argc, char *argv[])
-
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
 	{
 		printf("Error\n");
 		return (1);
 	}
 	num1 = _atoi(argv[1]);
-
 	num2 = _atoi(argv[2]);
-
 	result = num1 * num2;
-
 	printf("%d\n", result);
 	return (0);
-
 }
-
-
-
-
