ead7d4b91d2129dbb6d7b5e317c2552234ca6ede
emmanueleduke <eduke.emmanuel@gmail.com>
2023-01-18 01:04:55 -0800

    My header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,12 +5,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-
+int (*get_op_func(char *s))(int, int);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int _putchar(char c);
 
 
 #endif
+
